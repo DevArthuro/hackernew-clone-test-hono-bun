65f2a55574f1658ec545f53774abdf792b170459
Carlos Orrego <carlosorrego5151@gmail.com>
2024-10-22 23:55:19 -0500

    feat: add endpoint post pagination

--- common/types.ts
+++ common/types.ts
@@ -31,3 +31,38 @@ export const createPostSchema = insertpostSchema
     message: "Either URL or Content must be provided",
     path: ["url", "content"],
   });
+
+export const sortBySchema = z.enum(["points", "recently"]);
+export const orderSchema = z.enum(["asc", "desc"]);
+
+export const paginationSchema = z.object({
+  limit: z.number({ coerce: true }).optional().default(10),
+  page: z.number({ coerce: true }).optional().default(1),
+  sortBy: sortBySchema.optional().default("points"),
+  order: orderSchema.optional().default("desc"),
+  author: z.optional(z.string()),
+  site: z.string().optional(),
+});
+
+export type Post = {
+  id: number;
+  title: string;
+  url: string | null;
+  content: string | null;
+  points: number;
+  createdAt: string;
+  commentCount: number;
+  author: {
+    id: string;
+    username: string;
+  };
+  isUpvoted: boolean;
+};
+
+export type PaginatedResponse<T> = {
+  pagination: {
+    page: number;
+    totalPages: number;
+  };
+  data: T;
+} & Omit<SuccessResponse, "data">;
--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -1,18 +1,25 @@
 import { Hono } from "hono";
+import { and, asc, countDistinct, desc, eq, sql } from "drizzle-orm";
 
 import { db } from "@/adapter";
 import type { Context } from "@/context";
+import { userTable } from "@/db/schemas/auth";
 import { postsTable } from "@/db/schemas/posts";
+import { postUpvotesTable } from "@/db/schemas/upvotes";
 import { loggedIn } from "@/middleware/loggedIn";
 import { zValidator } from "@hono/zod-validator";
 
-import { createPostSchema, type SuccessResponse } from "@/common/types";
+import {
+  createPostSchema,
+  paginationSchema,
+  type PaginatedResponse,
+  type Post,
+  type SuccessResponse,
+} from "@/common/types";
+import { getISOFormatDateQuery } from "@/lib/utils";
 
-export const postRouter = new Hono<Context>().post(
-  "/",
-  loggedIn,
-  zValidator("form", createPostSchema),
-  async (c) => {
+export const postRouter = new Hono<Context>()
+  .post("/", loggedIn, zValidator("form", createPostSchema), async (c) => {
     const { title, content, url } = c.req.valid("form");
     const user = c.get("user")!;
     const [post] = await db
@@ -35,5 +42,78 @@ export const postRouter = new Hono<Context>().post(
       },
       201,
     );
-  },
-);
+  })
+  .get("/", zValidator("query", paginationSchema), async (c) => {
+    const { limit, order, page, sortBy, author, site } = c.req.valid("query");
+    const user = c.get("user");
+
+    const offset = (page - 1) * limit;
+
+    const sortByColumn =
+      sortBy === "points" ? postsTable.points : postsTable.createdAt;
+
+    const sortOrder = order === "desc" ? desc(sortByColumn) : asc(sortByColumn);
+
+    const [count] = await db
+      .select({ count: countDistinct(postsTable.id) })
+      .from(postsTable)
+      .where(
+        and(
+          author ? eq(postsTable.userId, author) : undefined,
+          site ? eq(postsTable.url, site) : undefined,
+        ),
+      );
+
+    const postsQuery = db
+      .select({
+        id: postsTable.id,
+        title: postsTable.title,
+        url: postsTable.url,
+        points: postsTable.points,
+        createdAt: getISOFormatDateQuery(postsTable.createdAt),
+        commentCount: postsTable.commentCount,
+        author: {
+          username: userTable.username,
+          id: userTable.id,
+        },
+        isUpvoted: user
+          ? sql<boolean>`CASE WHEN ${postUpvotesTable.userId} IS NOT NULL THEN true ELSE false END`
+          : sql<boolean>`false`,
+      })
+      .from(postsTable)
+      .leftJoin(userTable, eq(postsTable.userId, userTable.id))
+      .orderBy(sortOrder)
+      .limit(limit)
+      .offset(offset)
+      .where(
+        and(
+          author ? eq(postsTable.userId, author) : undefined,
+          site ? eq(postsTable.url, site) : undefined,
+        ),
+      );
+
+    if (user) {
+      postsQuery.leftJoin(
+        postUpvotesTable,
+        and(
+          eq(postUpvotesTable.postId, postsTable.id),
+          eq(postUpvotesTable.userId, user.id),
+        ),
+      );
+    }
+
+    const posts = (await postsQuery) as Post[];
+
+    return c.json<PaginatedResponse<Post[]>>(
+      {
+        data: posts,
+        success: true,
+        message: "Posts fetched",
+        pagination: {
+          page: page,
+          totalPages: Math.ceil(count.count / limit),
+        },
+      },
+      200,
+    );
+  });
